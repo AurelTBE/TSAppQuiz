f4180d9dcea0a9ad2800199f765bc9bf369c1e7c
aureltbe <aureltrouble@gmail.com>
2021-12-14 15:36:16 +0100

    WIP 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { Container, Grid } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import { StyledButtonTrue, StyledButtonFalse } from "./style";
 import { connect } from "react-redux";
-import { ThunkDispatch } from "redux-thunk";
 import { IStore } from "./reducers";
-import { getQuizListItem, giveAnswer } from "./actions/quiz"
-import { IQuizList, IQuizListItem } from "./models";
+import { getQuizListItem, giveAnswer, restart } from "./actions/quiz"
+import { IQuizListItem } from "./models";
 import { getCurrentQuizListItem } from "./selectors/quiz";
+import SnackBar from "@material-ui/core/Snackbar"
 
 interface OwnProps {
 
@@ -23,11 +23,21 @@ interface StateProps {
 interface DispatchProps {
   getQuizListItem: typeof getQuizListItem,
   giveAnswer: typeof giveAnswer
+  restart: typeof restart
+}
+
+interface LocalStateProps {
+  message: string,
+  isOpen: boolean,
+  className: "good"|"wrong"
 }
 
 type Props = OwnProps & StateProps & DispatchProps
 
-export class App extends Component<Props> {
+export class App extends Component<Props, LocalStateProps> {
+
+  state: LocalStateProps = { message: "", isOpen: false, className: "good" }
+
   componentDidMount(){
     this.props.getQuizListItem(10, "easy")
   }
@@ -54,23 +64,52 @@ export class App extends Component<Props> {
     const isCorrectAnswer = answer === this.props.currentQuizListItem!.correct_answer
     const isLastQuestion = this.props.currentQuizListItemIndex === this.props.quizListLength - 1
     this.props.giveAnswer(isCorrectAnswer, isLastQuestion)
+    this.setState({ message: isCorrectAnswer ? "Well done !" : "Nope !...", className: isCorrectAnswer ? "good" : "wrong", isOpen: true })
   }
 
   private renderButton = () => {
+    if(this.props.currentQuizListItemIndex < this.props.quizListLength - 1) {
+      return (
+        <Grid container direction="row" alignItems="center" justify="space-evenly" >
+          <StyledButtonTrue onClick={this.answerQuestion("True")}>TRUE</StyledButtonTrue>
+          <StyledButtonFalse onClick={this.answerQuestion("False")}>FALSE</StyledButtonFalse>
+        </Grid>
+      )
+    }
     return (
-      <Grid container direction="row" alignItems="center" justify="space-evenly" >
-        <StyledButtonTrue onClick={this.answerQuestion("True")}>TRUE</StyledButtonTrue>
-        <StyledButtonFalse onClick={this.answerQuestion("False")}>FALSE</StyledButtonFalse>
+      <Grid container direction="column" alignItems="center" justify="center" >
+        <Box mt={10} style={{marginBottom:"5%"}} fontSize={20} className="txt"> Final score : {this.props.score} / {this.props.quizListLength}</Box>
+        <StyledButtonTrue onClick={this.props.restart}>RESTART</StyledButtonTrue>
       </Grid>
+    )
+  }
+
+  private onSnackBarClose = () => {
+    this.setState({ isOpen: false })
+  }
+
+  private renderContent = () => {
+    return (
+      <div>
+        {this.renderHeader()}
+        {this.props.currentQuizListItem && this.props.currentQuizListItemIndex < this.props.quizListLength - 1 && this.renderQuestionInfo()}
+        {this.renderButton()}
+        <SnackBar 
+          anchorOrigin={{ vertical: "bottom", horizontal: "center" }} 
+          autoHideDuration={400}
+          open={this.state.isOpen}
+          onClose={this.onSnackBarClose}
+          message={this.state.message}
+          className={this.state.className}
+        />
+      </div>
     )
   }
 
   render() {
     return (
       <Container maxWidth="lg" >
-        {this.renderHeader()}
-        {this.props.currentQuizListItem && this.renderQuestionInfo()}
-        {this.renderButton()}
+        {this.props.currentQuizListItem! && this.renderContent()}
       </Container >
     );
   }
@@ -87,7 +126,8 @@ const mapStateToProps = (state: IStore): StateProps => {
 
 const mapDispatchToProps: any = {
   getQuizListItem,
-  giveAnswer
+  giveAnswer,
+  restart
 } 
 
 export default connect<StateProps, DispatchProps, OwnProps, IStore>(mapStateToProps, mapDispatchToProps)(App)
